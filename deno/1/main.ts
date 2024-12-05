23d418d1c234af2b38a3bd4375fec2aa90dfd79b
William Aperance <aperance@gmail.com>
2024-12-05 16:47:19 -0700

    Deno day 1

--- deno/1/main.ts
+++ deno/1/main.ts
@@ -1,1 +1,24 @@
-console.log("Hello World");
+const raw = await Deno.readTextFile("../../inputs/1/full.txt");
+
+const arr1: number[] = [];
+const arr2: number[] = [];
+
+for (const row of raw.split(/\n/)) {
+  const vals = row.split(/\s+/);
+
+  if (vals.length !== 2) {
+    throw Error("Row parsing error");
+  }
+
+  arr1.push(parseInt(vals[0]));
+  arr2.push(parseInt(vals[1]));
+}
+
+arr1.sort((a, b) => a - b);
+arr2.sort((a, b) => a - b);
+
+const diff = arr1.map((val1, i) => Math.abs(val1 - arr2[i]));
+const sum = diff.reduce((arr, val) => arr + val, 0);
+
+console.log({ arr1, arr2, diff });
+console.log("Final Result:", sum);
