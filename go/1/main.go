62b56aa8fc4c3ed1f33c3da45da7768ebcbb1bf5
William Aperance <aperance@gmail.com>
2024-12-05 18:46:24 -0700

    Go day 1

--- go/1/main.go
+++ go/1/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world")
+	file, err := os.Open("../../inputs/1/full.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	arr := make([][]int, 2)
+
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), "   ")
+		if len(row) != 2 {
+			log.Fatal("Row parsing error")
+		}
+
+		for i, str := range row {
+			num, err := strconv.Atoi(str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			arr[i] = append(arr[i], num)
+		}
+	}
+
+	sort.Ints(arr[0])
+	sort.Ints(arr[1])
+
+	sum := 0
+
+	for i, v0 := range arr[0] {
+		v1 := arr[1][i]
+		if v0 < v1 {
+			sum += v1 - v0
+		} else {
+			sum += v0 - v1
+		}
+	}
+
+	fmt.Println("Result:", sum)
 }
