10b348c025dd241f986ccb30f680f652c1fecdec
William Aperance <aperance@gmail.com>
2024-12-10 00:27:33 -0700

    Day 5

---
+++ go/5/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/aperance/advent-of-code-2024/go/pkg/stdin"
+)
+
+func checkUpdates(pages []string, ruleMap map[string][]string) bool {
+	for i := range pages {
+		for j := i + 1; j < len(pages); j++ {
+			f := slices.Contains(ruleMap[pages[i]], pages[j])
+			if !f {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	ruleMap := make(map[string][]string)
+	parsingRules := true
+	sum := 0
+	correctedSum := 0
+
+	scanner := stdin.GetScanner()
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			parsingRules = false
+			continue
+		}
+
+		if parsingRules {
+			rule := strings.Split(line, "|")
+			ruleMap[rule[0]] = append(ruleMap[rule[0]], rule[1])
+			continue
+		}
+
+		pages := strings.Split(line, ",")
+		ok := checkUpdates(pages, ruleMap)
+
+		if !ok {
+			sort.Slice(pages, func(i, j int) bool {
+				return slices.Contains(ruleMap[pages[i]], pages[j])
+			})
+		}
+
+		centerValue, err := strconv.Atoi(pages[len(pages)/2])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if ok {
+			sum += centerValue
+		} else {
+			correctedSum += centerValue
+		}
+	}
+
+	fmt.Println("Sum of passing center pages:", sum)
+	fmt.Println("Sum of corrected center pages:", correctedSum)
+}
