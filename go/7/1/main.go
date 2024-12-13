d91854a8288cf36cd702aae621c9da93e9acd9ba
William Aperance <aperance@gmail.com>
2024-12-13 15:17:13 -0700

    Go day 7

---
+++ go/7/1/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/aperance/advent-of-code-2024/go/pkg/stdin"
+)
+
+func checkEquation(operands []int, result int) bool {
+	matching := false
+	operationCount := len(operands) - 1
+
+	// Iterate over range of values where max number of bits equals number of operations.
+	// This represents all operation combinations (0 = addition and 1 = multiplication).
+	for i := 0; i < 1<<operationCount; i++ {
+		accumulator := operands[0]
+
+		// Iterate over operand positions
+		for j := 0; j < operationCount; j++ {
+			// If bit value of i at position j is 0 use addition, otherwise multiplication
+			if i&(1<<j) == 0 {
+				accumulator = accumulator + operands[j+1]
+			} else {
+				accumulator = accumulator * operands[j+1]
+			}
+		}
+
+		if accumulator == result {
+			matching = true
+		}
+	}
+
+	return matching
+}
+
+func main() {
+	sum := 0
+
+	scanner := stdin.GetScanner()
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ": ")
+
+		result, err := strconv.Atoi(row[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		operands := []int{}
+		for _, str := range strings.Split(row[1], " ") {
+			operand, err := strconv.Atoi(str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			operands = append(operands, operand)
+		}
+
+		matching := checkEquation(operands, result)
+		if matching {
+			sum += result
+		}
+	}
+
+	fmt.Println("Sum of matching equations:", sum)
+}
---
+++ go/7/2/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aperance/advent-of-code-2024/go/pkg/stdin"
+)
+
+func checkEquation(operands []int, operations []int, expectedResult int) bool {
+	if len(operations) == len(operands)-1 {
+		accumulator := operands[0]
+
+		for i, op := range operations {
+			if op == 0 {
+				accumulator = accumulator + operands[i+1]
+			} else if op == 1 {
+				accumulator = accumulator * operands[i+1]
+			} else {
+				concat, err := strconv.Atoi(strconv.Itoa(accumulator) + strconv.Itoa(operands[i+1]))
+				if err != nil {
+					log.Fatal(err)
+				}
+				accumulator = concat
+			}
+		}
+
+		return accumulator == expectedResult
+	} else {
+		for i := 0; i < 3; i++ {
+			newOperations := append(operations, i)
+			result := checkEquation(operands, newOperations, expectedResult)
+			if result {
+				return true
+			}
+		}
+
+		return false
+	}
+}
+
+func main() {
+	start := time.Now()
+
+	sum := 0
+
+	scanner := stdin.GetScanner()
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ": ")
+
+		result, err := strconv.Atoi(row[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		operands := []int{}
+		for _, str := range strings.Split(row[1], " ") {
+			operand, err := strconv.Atoi(str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			operands = append(operands, operand)
+		}
+
+		matching := checkEquation(operands, []int{}, result)
+		if matching {
+			sum += result
+		}
+	}
+
+	fmt.Println("Sum of matching equations:", sum)
+
+	elapsed := time.Since(start)
+	fmt.Println("Elapsed time:", elapsed)
+}
