3fbbc0c0eccbf146b1c77c11fdc2c460a521c279
William Aperance <aperance@gmail.com>
2024-12-18 21:10:35 -0700

    Day 18 part 1 animation

--- go/18/main.go
+++ go/18/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 	"strings"
 
 	"github.com/aperance/advent-of-code-2024/go/pkg/utils"
@@ -22,26 +24,27 @@ type memorySpace struct {
 	queue [][2]int
 }
 
-// func (s *memorySpace) print() {
-// 	for y := 0; y < size; y++ {
-// 		var row []string
-// 		for x := 0; x < size; x++ {
-// 			c := s.cells[x][y]
-// 			if x == size-1 && y == size-1 {
-// 				row = append(row, "E")
-// 			} else if c.corrupt {
-// 				row = append(row, "#")
-// 			} else if c.visited {
-// 				row = append(row, strconv.Itoa(c.distance))
-// 			} else {
-// 				row = append(row, ".")
-// 			}
-// 		}
-// 		fmt.Println(row)
-// 	}
-// }
+func (s *memorySpace) print() {
+	fmt.Print("\033[H\n")
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			c := s.cells[x][y]
+			if c.visited {
+				fmt.Print("\033[48;5;" + strconv.Itoa(c.distance%6+1) + "m\033[30m")
+				fmt.Print(" " + strconv.Itoa(c.distance%10) + " ")
+			} else if c.corrupt {
+				fmt.Print("\033[48;5;0m   ")
+			} else {
+				fmt.Print("\033[48;5;7m   ")
+			}
+			fmt.Print("\033[0m")
+		}
+		fmt.Print("\n")
+	}
+	fmt.Print("\n")
+}
 
-func (s *memorySpace) run() int {
+func (s *memorySpace) run(animate bool) int {
 	vectors := [4][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 
 	for {
@@ -53,6 +56,10 @@ func (s *memorySpace) run() int {
 		s.queue = s.queue[1:]
 
 		ancestorCell := &s.cells[ancestor[0]][ancestor[1]]
+
+		if ancestor[0] == size-1 && ancestor[1] == size-1 {
+			return ancestorCell.distance
+		}
 
 		for _, v := range vectors {
 			x := ancestor[0] + v[0]
@@ -70,11 +77,11 @@ func (s *memorySpace) run() int {
 			cell.visited = true
 			cell.distance = ancestorCell.distance + 1
 
-			if x == size-1 && y == size-1 {
-				return cell.distance
-			}
-
 			s.queue = append(s.queue, [2]int{x, y})
+		}
+
+		if animate {
+			s.print()
 		}
 	}
 }
@@ -82,6 +89,18 @@ func (s *memorySpace) run() int {
 func main() {
 	t := utils.StartTimer()
 	defer t.PrintDuration()
+
+	animate := false
+	if len(os.Args) > 1 && os.Args[1] == "--animate" {
+		animate = true
+	}
+
+	if animate {
+		fmt.Print("\033[2J\033[?25l")
+		f := func() { fmt.Print("\033[0m\033[?25h") }
+		utils.SetCleanup(f)
+		defer f()
+	}
 
 	space := memorySpace{}
 	space.cells[0][0] = memoryCell{visited: true, distance: 0}
@@ -100,7 +119,6 @@ func main() {
 		}
 	}
 
-	result := space.run()
+	result := space.run(animate)
 	fmt.Println("Found Path, distance:", result)
-	// space.print()
 }
